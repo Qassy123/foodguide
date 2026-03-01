735d7b0f317ee3ca347b3bac8d2736bf11f743fc
Qassy123 <Q.shah@wlv.ac.uk>
2026-03-01 10:42:16 +0000

    Created TakeawayModel for takeaways table

--- app/Models/TakeawayModel.php
+++ app/Models/TakeawayModel.php
@@ -6,7 +6,25 @@ use CodeIgniter\Model;
 
 class TakeawayModel extends Model
 {
+    // Defines the database table
     protected $table = 'takeaways';
+
+    // Defines the primary key
     protected $primaryKey = 'id';
-    protected $allowedFields = ['name', 'category', 'description'];
+
+    // Allows auto incrementing IDs
+    protected $useAutoIncrement = true;
+
+    // Defines return type as array
+    protected $returnType = 'array';
+
+    // Allows these fields to be inserted or updated
+    protected $allowedFields = [
+        'name',
+        'cuisine_type',
+        'address',
+        'price_range',
+        'rating',
+        'description'
+    ];
 }
